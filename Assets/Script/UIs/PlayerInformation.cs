66e738ea53631f63d3e868d1af295707a47a3698
StudentBuchun <1971045558@qq.com>
2023-10-01 21:42:51 -0600

    Update 10/1 21:42
    About Invenotry(70% done)

--- Assets/Script/UIs/PlayerInformation.cs
+++ Assets/Script/UIs/PlayerInformation.cs
@@ -12,8 +12,9 @@ public class PlayerInformation : MonoBehaviour
     void Update()
     {
         var playerinformation = GameObject.Find("Player").GetComponent<CharacterStats>();
-        healthText.text = string.Format("Health:{0}",playerinformation.characterData.maxHealth);
-        attackText.text = string.Format("Attack:{0}", playerinformation.attackData.damage);
+        //var weaponLeg = GameObject.Find("Player").GetComponent<Weapon>
+        healthText.text = string.Format("Health:{0}/{1}",playerinformation.characterData.currentHealth, playerinformation.characterData.maxHealth);
+        //attackText.text = string.Format("Attack:L:{0} R:{1}", playerinformation.attackData.damage);
         defenceText.text = string.Format("Defence:{0}", playerinformation.characterData.currentDefence);
     }
 }
