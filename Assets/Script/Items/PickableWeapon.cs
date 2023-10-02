ea40c0a9611cb1ab6c074b23828d53607bd39477
Selaginella742 <71528648+Selaginella742@users.noreply.github.com>
2023-10-02 01:27:42 -0600

    Update 10/2 1:24
    - add weapon pick up

--- Assets/Script/Items/PickableWeapon.cs
+++ Assets/Script/Items/PickableWeapon.cs
@@ -9,33 +9,48 @@ public class PickableWeapon : MonoBehaviour
     [TextArea] public string description;
 
     CharacterData_SO playerStat;
+    float sineCalc;
+    Vector3 anchorPos;
 
     private void Start()
     {
         var player = GameObject.FindGameObjectWithTag("Player");
         playerStat = player.GetComponent<CharacterStats>().characterData;
+        sineCalc = 0f;
+        anchorPos = transform.position;
     }
-    void Update()
+    void FixedUpdate()
     {
         //the rotation when the item is dropped
         Vector3 currentAngle = transform.rotation.eulerAngles;
-        currentAngle.y += 1;
+        currentAngle.y += 0.5f;
         transform.rotation = Quaternion.Euler(currentAngle);
+
+        sineCalc += 0.05f;
+
+
+        transform.position = new Vector3(anchorPos.x, anchorPos.y + Mathf.Sin(sineCalc), anchorPos.z);
+
+        if (sineCalc >= Mathf.PI * 2)
+            sineCalc = 0;
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerStay(Collider collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            if (Input.GetKey(KeyCode.LeftControl) && Input.GetMouseButton(0))
+            Debug.Log("CTRL + mouse to change");
+            if (Input.GetKey(KeyCode.LeftControl))
             {
-                playerStat.leftWeaponIndex = weaponIndex;
-                Destroy(this.gameObject);
-            }
-            else if (Input.GetKey(KeyCode.LeftControl) && Input.GetMouseButton(1))
-            {
-                playerStat.rightWeaponIndex = weaponIndex;
-                Destroy(this.gameObject);
+                if (Input.GetMouseButton(0))
+                {
+                    playerStat.leftWeaponIndex = weaponIndex;
+                    Destroy(this.gameObject);
+                } else if (Input.GetMouseButton(1))
+                {
+                    playerStat.rightWeaponIndex = weaponIndex;
+                    Destroy(this.gameObject);
+                }
             }
 
         }
