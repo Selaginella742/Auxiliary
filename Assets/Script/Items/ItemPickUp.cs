dc4fe423b63460aba2b09b6d2364ae278d70b085
StudentBuchun <1971045558@qq.com>
2023-11-18 16:52:01 -0700

    Update 2023/11/18 16:51
    -Random Item pickup

--- Assets/Script/Items/ItemPickUp.cs
+++ Assets/Script/Items/ItemPickUp.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 public class ItemPickUp : MonoBehaviour
 {
 
-    public ItemData_SO itemData;
+    public ItemData_SO[] itemDatas;
+    private ItemData_SO itemData;
+
+    void RandomItem()
+    {
+        itemData = itemDatas[Random.Range(0, itemDatas.Length)];
+    }
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             //TODO:put item into inventory
+            RandomItem();
             InventoryManager.Instance.AddItemToInventory(itemData);
             Destroy(gameObject);
             FindObjectOfType<AudioManager>().Play("ItemPickUp");
